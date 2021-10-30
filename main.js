caa4b14a75094cc362c40d69f316f91a665bf89c
Caio Victor Rafael <caio-victor-rafael@hotmail.com>
2021-10-29 22:03:20 -0300

    Adicionado footer e um pouco de javascript

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const nav = document.querySelector('#header nav');
 const toggle = document.querySelectorAll('nav .toggle');
 
 for (const element of toggle) {
-  element.addEventListener('click', function() {
+  element.addEventListener('click', function () {
     nav.classList.toggle('show');
   });
 }
@@ -18,18 +18,20 @@ for (const link of links) {
 }
 
 /* mudar o header da pagina quando der scroll */
-const header = document.querySelector('#header');
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header');
+  const navHeight = header.offsetHeight;
+
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll');
-  }else {
+  } else {
     // menor que a altura do header
     header.classList.remove('scroll');
   }
-});
+}
+
 
 /* ScrollReveal: Mostrar elementos quando der scroll na pagina */
 const scrollReveal = ScrollReveal({
@@ -40,10 +42,30 @@ const scrollReveal = ScrollReveal({
 });
 
 scrollReveal.reveal(
-`#home .image, #home .text,
- #about .image, #about .text,
- #services header, #services .card,
- #testimonials header, #testimonials .testimonials
- #contact .text, #contact .links`,
- { interval: 100}
- );
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social 
+  `,
+  { interval: 100 }
+);
+
+/* botao voltar para o topo */
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top');
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+};
+
+/* when Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll();
+  backToTop();
+});
