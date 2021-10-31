751344c667c6f169246070fd7c7283bdec68af2d
Caio Victor Rafael <caio-victor-rafael@hotmail.com>
2021-10-31 14:36:48 -0300

    Pagina finalizada. Criado versão para desktop, e algumas atualizacoes

--- main.js
+++ main.js
@@ -64,8 +64,35 @@ function backToTop() {
   }
 };
 
+/* Menu ativo conforme a seção visível na pagina */
+const sections = document.querySelectorAll('main section[id]');
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active');
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active');
+    }
+  }
+
+}
+
 /* when Scroll */
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll();
   backToTop();
+  activateMenuAtCurrentSection();
 });
